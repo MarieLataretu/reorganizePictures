8850999cd74bb5961925841e244ac4067ccd8028
MarieLataretu <marie.lataretu@uni-jena.de>
2021-11-07 21:48:39 +0100

    Sorting options Fixes #3

--- reorganize.py
+++ reorganize.py
@@ -14,17 +14,22 @@ def log(string):
     sys.stderr.write(f'LOG: {string}\n')
 
 jpg_file_extensions = ['jpg', 'jpeg', 'jpe', 'jfif']
+dir_pattern = '%m-%d'
 
-parser = argparse.ArgumentParser(description=f"Reorganize jpg files in a directory, so that:\ntarget\n|___year\n|   |___month-day\n\nRecognizable file extensions: {', '.join(jpg_file_extensions)} (not case sensitive).", formatter_class=RawTextHelpFormatter)
+parser = argparse.ArgumentParser(description=f"Reorganize jpg files in a directory, so that:\ntarget\n|___year\n|   |___PATTERN\nDefault pattern: 'month-day'\nCan be changed with -p to any strftime format code sting\n(see https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes).\n\nRecognizable file extensions: {', '.join(jpg_file_extensions)} (not case sensitive).", formatter_class=RawTextHelpFormatter)
 parser.add_argument('mode', choices=['cp', 'mv'], help='copy (cp) or move (mv) files')
 parser.add_argument('source', help='source directory')
 parser.add_argument('target', help='parent directory for the reorganized files')
-parser.add_argument('-f', '--force', action='store_true', default=False, help='overwrite file if exist')
+parser.add_argument('-p', '--pattern', help='subdirectory naming pattern (strftime format code sting) [%%m-%%d]')
+parser.add_argument('-f', '--force', action='store_true', default=False, help='overwrite file if exist [false]')
 
 args = parser.parse_args()
 
 if not os.path.isdir(args.source):
 	error(f"{args.source} does not exist.")
+
+if args.pattern:
+	dir_pattern = args.pattern
 
 for dirpath,_,filenames in os.walk(args.source):
 	for f in filenames:
@@ -44,7 +49,7 @@ for dirpath,_,filenames in os.walk(args.source):
 
 				if (date):
 					timeStamp = datetime.strptime(date.__str__(), '%Y:%m:%d %H:%M:%S')
-					full_Date = timeStamp.strftime('%m-%d')
+					full_Date = timeStamp.strftime(dir_pattern)
 					year = str(timeStamp.year)
 
 					target_dir_with_date = f"{args.target}/{year}/{full_Date}"
